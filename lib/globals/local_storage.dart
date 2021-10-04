803a09f87d4984244818b3430b96bb16d502013c
mclt0568 <tis7bfrankie@gmail.com>
2021-10-04 20:04:08 +0800

    add wrapper to store key-value in localstorage

---
+++ lib/globals/local_storage.dart
@@ -0,0 +1,12 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorage{
+  static setItem(String key, String value) async {
+    SharedPreferences s = await SharedPreferences.getInstance();
+    s.setString(key, value);
+  }
+  static getItem(String key) async {
+    SharedPreferences s = await SharedPreferences.getInstance();
+    return s.getString(key);
+  }
+}
