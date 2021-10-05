e7d0b86766c9ed2acce459852054f1554de4cc96
mclt0568 <tis7bfrankie@gmail.com>
2021-10-06 06:35:24 +0800

    add size support to paragraph

--- lib/components/paragraph.dart
+++ lib/components/paragraph.dart
@@ -4,9 +4,10 @@ import 'package:monolibro/globals/theme_colors.dart';
 import 'package:monolibro/globals/typography.dart' as T;
 
 class Paragraph extends StatelessWidget{
-  const Paragraph({Key? key, this.text, this.color}) : super(key: key);
+  const Paragraph({Key? key, this.text, this.color, this.size}) : super(key: key);
   final String? text;
   final Color? color;
+  final double? size;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class Paragraph extends StatelessWidget{
       text ?? "",
       style: TextStyle(
         fontFamily: T.Typography.latinFontPrimary,
-        fontSize: T.Typography.paragraphSize,
+        fontSize: size ?? T.Typography.paragraphSize,
         color: color ?? ThemeColors.grayAccent[3],
         decoration: TextDecoration.none,
         fontWeight: FontWeight.normal
