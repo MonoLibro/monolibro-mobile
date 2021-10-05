30bde36936dc630c61977005b1b48f8f86899031
mclt0568 <tis7bfrankie@gmail.com>
2021-10-06 06:36:30 +0800

    add account init page

---
+++ lib/pages/initialization_pages/account_init_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:monolibro/components/focus_scaffold.dart';
+import 'package:monolibro/components/large_icon_button.dart';
+import 'package:monolibro/components/paragraph.dart';
+import 'package:monolibro/globals/internationalization.dart';
+import 'package:monolibro/globals/theme_colors.dart';
+import 'package:monolibro/globals/typography.dart' as T;
+
+class AccountInitPage extends StatefulWidget{
+  const AccountInitPage({Key? key}) : super(key: key);
+
+  @override
+  _AccountInitPageState createState() => _AccountInitPageState();
+}
+
+class _AccountInitPageState extends State<AccountInitPage>{
+  Map text = {};
+  final promptSequence = ["hasAccount","newToApp"];
+  final buttonSequence = ["importAccount","newAccount"];
+  final buttonIconSequence = [
+    FaIcon(
+      FontAwesomeIcons.fileImport,
+      color: ThemeColors.grayAccent[0],
+      size: 40,
+    ),
+    Icon(
+      Icons.person_add,
+      color: ThemeColors.grayAccent[0],
+      size: 40,
+    )
+  ];
+
+  String getText(String key){
+    if (text == {}) return "";
+    return text[key].toString();
+  }
+
+  getData() async {
+    await Internationalization.setLanguage("en");
+    var result = await Internationalization.getTranslationObject(context, "account_init_page");
+    setState(() {
+      text = result;
+    });
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Widget buildOption(int index){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 15),
+          child: Paragraph(text:getText(promptSequence[index])),
+        ),
+        LargeIconButton(
+          text: getText(buttonSequence[index]),
+          icon: buttonIconSequence[index],
+          onTap: (){},
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.white,
+      child: FocusScaffold(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                getText("getStarted"),
+                style: TextStyle(
+                  fontFamily: T.Typography.latinFontPrimary,
+                  color: ThemeColors.defaultAccent[3],
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildOption(0),
+                  buildOption(1),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// class AccountInitPage extends StatelessWidget{
+//   const AccountInitPage({Key? key}) : super(key: key);
+
+//   Map text = {},
+
+//   Future<void> getData() async {
+
+//   }
+
+//   init
+
+//   
+// }
