80e4ba0780eebd4ae68a0dcccd93c72f28529d9a
mclt0568 <tis7bfrankie@gmail.com>
2021-10-05 20:19:18 +0800

    add custom button

---
+++ lib/components/button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'package:monolibro/globals/theme_colors.dart';
+import 'package:monolibro/globals/typography.dart' as ThemeTypography;
+
+class Button extends StatelessWidget{
+  const Button({Key? key, required this.text, required this.onPressed}) : super(key: key);
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      decoration: BoxDecoration(
+        color: ThemeColors.defaultAccent[3],
+        boxShadow: [
+          BoxShadow(
+            color: ThemeColors.defaultButtonShadow,
+            blurRadius: 10,
+          )
+        ],
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(text),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
+          primary: Colors.white,
+          textStyle: TextStyle(
+            fontSize: ThemeTypography.Typography.paragraphSize,
+            fontFamily: ThemeTypography.Typography.latinFontPrimary,
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ),
+    );
+  }
+}
