5281cc35dfc73f8af0bbcad74d179189dad6fdea
mclt0568 <tis7bfrankie@gmail.com>
2021-10-08 23:04:19 +0800

    add input text

---
+++ lib/components/input_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:monolibro/globals/theme_colors.dart';
+
+class InputText extends StatelessWidget{
+  const InputText({Key? key, this.hint}) : super(key: key);
+  final String? hint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        height: 40,
+        child: TextField(
+          cursorColor: ThemeColors.defaultAccent[3],
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: ThemeColors.grayAccent[2]),
+            ),
+            hintText: hint,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: ThemeColors.defaultAccent[3]),
+            )
+          ),
+        )
+      ),
+    );
+  }
+}
