fc492f66f92fd5debb8c71e97d3a5a08c471dd34
mclt0568 <tis7bfrankie@gmail.com>
2021-10-10 00:52:05 +0800

    scaffold support for no title

--- lib/components/monolibro_scaffold.dart
+++ lib/components/monolibro_scaffold.dart
@@ -6,8 +6,8 @@ import 'package:monolibro/globals/theme_colors.dart';
 import 'package:monolibro/globals/typography.dart' as t;
 
 class MonolibroScaffold extends StatelessWidget{
-  const MonolibroScaffold({Key? key, required this.title, this.color, this.body, this.popButton, this.left, this.fullIcon}) : super(key: key);
-  final String title;
+  const MonolibroScaffold({Key? key, this.title, this.color, this.body, this.popButton, this.left, this.fullIcon}) : super(key: key);
+  final String? title;
   final Color? color;
   final Widget? body;
   final bool? popButton;
@@ -16,6 +16,7 @@ class MonolibroScaffold extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+    bool hasTitle = title != null;
     bool hasPopButton = popButton ?? true;
     bool hasFullIcon = fullIcon ?? false;
     return Stack(
@@ -23,65 +24,68 @@ class MonolibroScaffold extends StatelessWidget{
         Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          padding: const EdgeInsets.only(top: 178),
+          padding: EdgeInsets.only(top: hasTitle ? 178 : 110),
           color: color ?? const Color(0xFFFFFFFF),
           child: body,
         ),
-        Positioned(
-          top: 110,
-          child: Container(
-            height: 68,
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.fromLTRB(hasPopButton ? 10 : 25 , 10, 10, 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Visibility(
-                      visible: popButton ?? true,
-                      child: Container(
-                        margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                        child: Material(
-                          color: ThemeColors.defaultAccent[0],
-                          child: IconButton(
-                            onPressed: (){
-                              Navigator.pop(context);
-                            },
-                            color: ThemeColors.grayAccent[3],
-                            splashColor: ThemeColors.defaultAccent[1],
-                            focusColor: ThemeColors.defaultAccent[1],
-                            hoverColor: ThemeColors.defaultAccent[1],
-                            highlightColor: ThemeColors.defaultAccent[1],
-                            icon: const Icon(Icons.arrow_back_ios_new),
+        Visibility(
+          visible: hasTitle,
+          child: Positioned(
+            top: 110,
+            child: Container(
+              height: 68,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.fromLTRB(hasPopButton ? 10 : 25 , 10, 10, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Visibility(
+                        visible: popButton ?? true,
+                        child: Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                          child: Material(
+                            color: ThemeColors.defaultAccent[0],
+                            child: IconButton(
+                              onPressed: (){
+                                Navigator.pop(context);
+                              },
+                              color: ThemeColors.grayAccent[3],
+                              splashColor: ThemeColors.defaultAccent[1],
+                              focusColor: ThemeColors.defaultAccent[1],
+                              hoverColor: ThemeColors.defaultAccent[1],
+                              highlightColor: ThemeColors.defaultAccent[1],
+                              icon: const Icon(Icons.arrow_back_ios_new),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Text(
-                      title,
-                      style: TextStyle(
-                        fontSize: t.Typography.appbarTitleSize,
-                        decoration: TextDecoration.none,
-                        color: ThemeColors.grayAccent[3],
-                        fontFamily: t.Typography.latinFontPrimary,
-                        fontWeight: FontWeight.w200,
+                      Text(
+                        title ?? "",
+                        style: TextStyle(
+                          fontSize: t.Typography.appbarTitleSize,
+                          decoration: TextDecoration.none,
+                          color: ThemeColors.grayAccent[3],
+                          fontFamily: t.Typography.latinFontPrimary,
+                          fontWeight: FontWeight.w200,
+                        )
                       )
-                    )
-                  ],
-                ),
-                Container(
-                  child: left,
-                ),
-              ],
-            ),
-            decoration: BoxDecoration(
-              color: ThemeColors.defaultAccent[0],
-              boxShadow: [BoxShadow(
-                color: ThemeColors.defaultAppbarShadow,
-                spreadRadius: 2,
-                blurRadius: 5,
-              )],
+                    ],
+                  ),
+                  Container(
+                    child: left,
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: ThemeColors.defaultAccent[0],
+                boxShadow: [BoxShadow(
+                  color: ThemeColors.defaultAppbarShadow,
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                )],
+              ),
             ),
           ),
         ),
