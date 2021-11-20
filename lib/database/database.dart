88692cfe3bf19a51b3e66a9fae569b272b624289
mclt0568 <tis7bfrankie@gmail.com>
2021-11-20 15:25:26 +0800

    add db implementation

---
+++ lib/database/database.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseWrapper{
+  Database? db;
+  bool active = false;
+  
+  DatabaseWrapper(){
+    initialize();
+  }
+  
+  Future<void> initialize() async {
+    db = await openDatabase(
+      "db.sql",
+      onCreate: (Database db, int version) async {
+        // await db.execute('CREATE TABLE Users ()')
+      }
+    );
+    active = true;
+  }
+  
+  Future<void> close() async {
+    await db!.close();
+  }
+
+  Future<void> fullReset() async {
+    active = false;
+    if (active){
+      await close();
+    }
+    await deleteDatabase("db.sql");
+    await initialize();
+  }
+
+  Future<void> execute(String sql) async {
+    if (!active){
+      throw Exception("DB not active yet");
+    }
+    await db!.execute(sql);
+  }
+
+  Future<List<Map>> executeWithResult(String sql) async {
+    if (!active){
+      throw Exception("DB not active yet");
+    }
+    return db!.rawQuery(sql);
+  }
+}
