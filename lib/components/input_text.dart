3de9fbb2000775294724183d98361d478ffa1d0f
mclt0568 <tis7bfrankie@gmail.com>
2021-11-22 17:14:56 +0800

    add error indicator and controller to input_text

--- lib/components/input_text.dart
+++ lib/components/input_text.dart
@@ -1,29 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:monolibro/components/paragraph.dart';
 import 'package:monolibro/globals/theme_colors.dart';
 
 class InputText extends StatelessWidget {
-  const InputText({Key? key, this.hint}) : super(key: key);
+  InputText({Key? key, this.hint, this.controller, this.errorMessage, this.error}) : super(key: key);
+  final TextEditingController? controller;
   final String? hint;
+  final String? errorMessage;
+  final bool? error;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: SizedBox(
-          height: 40,
-          child: TextField(
-            cursorColor: ThemeColors.defaultAccent[3],
-            decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 40,
+            child: TextField(
+              controller: controller,
+              cursorColor: ThemeColors.defaultAccent[3],
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
                 border: OutlineInputBorder(
-                  borderSide: BorderSide(color: ThemeColors.grayAccent[2]),
+                  borderSide: BorderSide(color: (error??false) ? ThemeColors.errorAccent[0] : ThemeColors.grayAccent[2]),
                 ),
                 hintText: hint,
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: ThemeColors.defaultAccent[3]),
-                )),
-          )),
+                  borderSide: BorderSide(color: (error??false) ? ThemeColors.errorAccent[0] : ThemeColors.defaultAccent[3]),
+                )
+              ),
+            )
+          ),
+          Visibility(
+            visible: error ?? false,
+            child: Paragraph(
+              text: errorMessage ?? "",
+              color: ThemeColors.errorAccent[0]
+            )
+          )
+        ],
+      )
     );
   }
 }
