3cc7b5c32f3f13977131a8c04dfdd0f792eaf22d
mclt0568 <tis7bfrankie@gmail.com>
2021-11-22 21:23:20 +0800

    removed debug page

--- lib/monolibro/state.dart
+++ lib/monolibro/state.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
+import 'package:monolibro/monolibro/models/activity.dart';
 import 'package:monolibro/monolibro/voting_session.dart';
 
 class State{
   Map<String, VotingSession> votingSessions = {};
-  StreamController<String> debugStream = StreamController<String>();
 }
